7833e33bd02ffc84d0516efe92c6f8568c4abee0
Brian Alvarez <brianalvarez2000@gmail.com>
2024-07-29 07:13:57 -0500

    add Dockerfile

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ import { envVars } from './config';
 import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 
 async function bootstrap() {
-  const logger = new Logger('Main');
+  const logger = new Logger('Main-Products');
 
   const app = await NestFactory.createMicroservice<MicroserviceOptions>(
     AppModule,
